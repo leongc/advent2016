a60e6bfc737c18c716216159821b971e7550e9e2
Cheng Leong <leongc@alumni.rice.edu>
2018-12-03 04:18:09 -0600

    Update day05.js
    part 2

--- day05.js
+++ day05.js
@@ -34,5 +34,47 @@ function getPassword(prefix, len) {
   }
   return password;
 }
-console.assert(getPassword("abc", 8) == "18f47a30");
-console.log(getPassword("cxdnnyjw", 8));
+//console.assert(getPassword("abc", 8) == "18f47a30");
+//console.log(getPassword("cxdnnyjw", 8));
+
+/*
+--- Part Two ---
+As the door slides open, you are presented with a second door that uses a slightly more inspired security mechanism. Clearly unimpressed by the last version (in what movie is the password decrypted in order?!), the Easter Bunny engineers have worked out a better solution.
+
+Instead of simply filling in the password from left to right, the hash now also indicates the position within the password to fill. You still look for hashes that begin with five zeroes; however, now, the sixth character represents the position (0-7), and the seventh character is the character to put in that position.
+
+A hash result of 000001f means that f is the second character in the password. Use only the first result for each position, and ignore invalid positions.
+
+For example, if the Door ID is abc:
+
+The first interesting hash is from abc3231929, which produces 0000015...; so, 5 goes in position 1: _5______.
+In the previous method, 5017308 produced an interesting hash; however, it is ignored, because it specifies an invalid position (8).
+The second interesting hash is at index 5357525, which produces 000004e...; so, e goes in position 4: _5__e___.
+You almost choke on your popcorn as the final character falls into place, producing the password 05ace8e3.
+
+Given the actual Door ID and this new method, what is the password? Be extra proud of your solution if it uses a cinematic "decrypting" animation.
+*/
+
+function getPassword2(prefix) {
+  var c = 0;
+  var password = [];
+  password.length = 8;
+  var unknownPositions = new Set([0,1,2,3,4,5,6,7]);
+  while (unknownPositions.size > 0) {
+    var hash = md5(prefix + c);
+    if (hash.startsWith("00000")) {
+      var position = parseInt(hash[5]);
+      if (unknownPositions.has(position)) {
+        unknownPositions.delete(position);
+        password[position] = hash[6];
+        console.log(c + "\t" + password.toString());
+      }
+    }
+    c++;
+    if (c % 1000000 == 0) {
+      console.log(c);
+    }
+  }
+  return password;
+}
+console.log(getPassword2("cxdnnyjw", 8));
